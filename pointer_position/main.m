258b72076ddd7f176df55010c4380790e2b67baf
hiro@juzbox.com <hiro@juzbox.com>
2011-11-15 01:47:06 +0900

    Initial Commit

---
+++ pointer_position/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pointer_position
+//
+//  Created by Hiro on 11/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
